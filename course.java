3eb72f8df7eb26cd8306048a2a5896ea18e26ee7
Rafael Navarro <44828667+rmnavarro@users.noreply.github.com>
2019-09-05 00:34:53 -0500

    Create course.java

---
+++ course.java
@@ -0,0 +1,20 @@
+public class course {
+       
+    private int student_ID;  
+    private String course_name;
+    private String faculty_name;
+    
+    //Constructor
+    public courses(){  ;}
+    
+    //modifiers
+    public void change_student_ID(String input_student_ID) { student_ID = input_student_ID; }
+    public void change_course_name(String input_course_name) { course_name = input_course_name; }
+    public void change_faculty_name(String input_faculty_name) { faculty_name = input_faculty_name; }
+    
+    //retrievers
+    public String get_student_ID( return student_ID;)
+    public String get_course_name( return course_name;)
+    public String get_faculty_name( return faculty_name;)
+
+}
