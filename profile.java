5fb2c2e8205c5d325e3d40037b8f471c0756a6d1
Rafael Navarro <44828667+rmnavarro@users.noreply.github.com>
2019-09-05 00:21:53 -0500

    Update profile.java

--- profile.java
+++ profile.java
@@ -1,13 +1,16 @@
-public class profiles {
+public class profile {
     
     private String Name;
     private int numID;
+    private String address;
     
     //modifiers
     public void change_Name(String Input_Name){Name = Input_Name;}
     public void change_numID(int input_numID) {numID = input_numID;}
+    public void change_address(int input_address) {adress = input_address;}
     
     //retrievers 
     public String get_Name() { return Name; }
     public int get_numID() { return numID; }
+    public int get_address() { return address; }
 }
